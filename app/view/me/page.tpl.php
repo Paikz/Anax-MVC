ee50cabd85e9d3c428bcb5efcae3ae1d074026a7
Philip Esmailzade <philipesmailzade@gmail.com>
2016-04-04 00:21:18 +0200

    Removed logo from header. Changed markdown files aka content. Added byline picture to page.tpl.

--- app/view/me/page.tpl.php
+++ app/view/me/page.tpl.php
@@ -1,10 +1,12 @@
 <article class="article1">
-
+<div class="content">
 <?=$content?>
+</div>
 
 <?php if(isset($byline)) : ?>
 <footer class="byline">
 <?=$byline?>
+<img src="https://pbs.twimg.com/profile_images/595144564958048259/STbMvdZl.jpg" width="100" height="100" style="border-radius: 8px; display: inline-block;" />
 </footer>
 <?php endif; ?>
 
